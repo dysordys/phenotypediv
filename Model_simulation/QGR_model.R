950925f26ae71b7848e14dbb92a1a0c4dd8ca090
Gyorgy Barabas <dysordys@gmail.com>
2022-01-28 18:40:49 +0100

    Code clean-up; better extract_dynamical_variables() function in Model_simulations/diversity.R

--- Model_simulation/QGR_model.R
+++ Model_simulation/QGR_model.R
@@ -1,52 +1,51 @@
+library(deSolve)   # for solving differential equations
+library(mvtnorm)   # for multivariate normal distributions
+library(tidyverse) # for efficient data manipulation and plotting
 
-require(deSolve)   ## for solving differential equations
-require(mvtnorm)   ## for multivariate normal distributions
-require(tidyverse) ## for efficient data manipulation and plotting
+source("solve_eqs.R") # functions for integrating the ODEs
+source("plot.R") # functions for plotting results
+source("diversity.R") # functions for calculating functional diversity
 
-source("solve_eqs.R") ## functions for integrating the ODEs
-source("plot.R") ## functions for plotting results
-source("diversity.R") ## functions for calculating functional diversity
-
-set.seed(57310) ## set random seed for reproducibility
-S <- 3 ## number of species
-L <- 1 ## number of trait dimensions
-w <- 0.15 ## competition width
+set.seed(57310) # set random seed for reproducibility
+S <- 3 # number of species
+L <- 1 # number of trait dimensions
+w <- 0.15 # competition width
 W <- diag(rep(w, L)^2, nrow=L, ncol=L)/2
-E <- array(0, c(S, L, L)) ## species' environmental trait covariances
+E <- array(0, c(S, L, L)) # species' environmental trait covariances
 for (i in 1:S) E[i,,] <- diag(runif(L, 0.005, 0.008)^2, nrow=L, ncol=L)
-bshape <- "b_quadratic" ## shape of intrinsic growth function
-params <- list(W=W, E=E, bshape=bshape) ## list of parameters
+bshape <- "b_quadratic" # shape of intrinsic growth function
+params <- list(W=W, E=E, bshape=bshape) # list of parameters
 
-ninit <- rep(1, S) ## initial species densities
-minit <- matrix(runif(S*L, -0.5, 0.5), S, L) ## initial species trait means
-Ginit <- array(0, c(S, L, L)) ## initial genetic covariances
+ninit <- rep(1, S) # initial species densities
+minit <- matrix(runif(S*L, -0.5, 0.5), S, L) # initial species trait means
+Ginit <- array(0, c(S, L, L)) # initial genetic covariances
 for (i in 1:S) {
-    O <- qr.Q(qr(matrix(rnorm(L^2), L, L))) ## create random orthogonal matrix
-    B <- diag(runif(L, 0.01, 0.05)^2, nrow=L, ncol=L) ## random PC axis lengths
-    Ginit[i,,] <- O%*%B%*%t(O) ## merge decomposition
+  O <- qr.Q(qr(matrix(rnorm(L^2), L, L))) # create random orthogonal matrix
+  B <- diag(runif(L, 0.01, 0.05)^2, nrow=L, ncol=L) # random PC axis lengths
+  Ginit[i,,] <- O%*%B%*%t(O) # merge decomposition
 }
-ic <- c(ninit, minit, Ginit) ## merge initial conditions in one vector
+ic <- c(ninit, minit, Ginit) # merge initial conditions in one vector
 
-## solve ODEs for 1e10 time units, with the parameters defined above
+# solve ODEs for 1e10 time units, with the parameters defined above
 dat <- run_dynamics(ic, seq(0, 1e10, by=1e7), params) %>%
-    organize_results(params) ## put results in tidy table
+  organize_results(params) # put results in tidy table
 
-## in 1D and 2D, plot the final trait distributions
+# in 1D and 2D, plot the final trait distributions
 if (L==1) plot_snapshot(dat %>% filter(time==max(time)), limits=c(-0.6, 0.6, NA))
 if (L==2) plot_snapshot_2D(dat %>% filter(time==max(time)))
 
-## obtain data for the final time step
+# obtain data for the final time step
 final <- dat %>%
-    filter(time==max(time)) %>% ## only final community state
-    arrange(-n) %>% ## order rows in decreasing order of population density
-    mutate(n=ifelse(n<=1e-6, 0, n)) ## species below threshold density are extinct
+  filter(time==max(time)) %>% # only final community state
+  arrange(-n) %>% # order rows in decreasing order of population density
+  mutate(n=ifelse(n<=1e-6, 0, n)) # species below threshold density are extinct
 
-## calculate species diversity
+# calculate species diversity
 spdiv <- species_diversity(final$n, 2)
-## calculate functional diversity of order 2 (inverse Simpson index):
+# calculate functional diversity of order 2 (inverse Simpson index):
 funcdiv <- functional_diversity(final, 2, seq(-1, 1, l=101))
 
-## print results on screen
-print(final) ## final community state
-print(spdiv) ## species diversity
-print(funcdiv) ## functional diversity
+# print results on screen
+print(final) # final community state
+print(spdiv) # species diversity
+print(funcdiv) # functional diversity
--- Model_simulation/solve_eqs.R
+++ Model_simulation/solve_eqs.R
@@ -1,202 +1,199 @@
-
-## obtain b_i, g_i, and Q_i, assuming the intrinsic growth function is quadratic
-## Input:
-## - m: an SxL matrix; m[i,k] is the kth trait mean of species i
-## - P: an SxLxL array; P[i,k,l] is the (k,l)th covar matrix entry of species i
-## Output:
-## - a list with three named entries:
-##   * b: vector with S entries
-##   * g: an SxL matrix
-##   * Q: an SxLxL array
+# obtain b_i, g_i, and Q_i, assuming the intrinsic growth function is quadratic
+# Input:
+# - m: an SxL matrix; m[i,k] is the kth trait mean of species i
+# - P: an SxLxL array; P[i,k,l] is the (k,l)th covar matrix entry of species i
+# Output:
+# - a list with three named entries:
+#   * b: vector with S entries
+#   * g: an SxL matrix
+#   * Q: an SxLxL array
 b_quadratic <- function(m, P) {
-    S <- dim(m)[1]
-    L <- dim(m)[2]
-    b <- rep(0, S)
-    g <- matrix(0, S, L)
-    Q <- array(0, c(S, L, L))
-    for (i in 1:S) {
-        trP <- as.numeric(ifelse(L==1, P[i,,], sum(diag(P[i,,])))) ## trace(P_i)
-        mm <- sum(m[i,]^2) ## m_i %*% m_i
-        b[i] <- 1-(trP+mm)/0.5^2
-        g[i,] <- -2*m[i,]/0.5^2
-        Q[i,,] <- -2*diag(L)/0.5^2
-    }
-    return(list(b=b, g=g, Q=Q))
+  S <- dim(m)[1]
+  L <- dim(m)[2]
+  b <- rep(0, S)
+  g <- matrix(0, S, L)
+  Q <- array(0, c(S, L, L))
+  for (i in 1:S) {
+    trP <- as.numeric(ifelse(L==1, P[i,,], sum(diag(P[i,,])))) # trace(P_i)
+    mm <- sum(m[i,]^2) # m_i %*% m_i
+    b[i] <- 1-(trP+mm)/0.5^2
+    g[i,] <- -2*m[i,]/0.5^2
+    Q[i,,] <- -2*diag(L)/0.5^2
+  }
+  list(b=b, g=g, Q=Q)
 }
 
-## obtain b_i, g_i, and Q_i, assuming the intrinsic growth function is quartic
-## Input:
-## - m: an SxL matrix; m[i,k] is the kth trait mean of species i
-## - P: an SxLxL array; P[i,k,l] is the (k,l)th covar matrix entry of species i
-## Output:
-## - a list with three named entries:
-##   * b: vector with S entries
-##   * g: an SxL matrix
-##   * Q: an SxLxL array
+# obtain b_i, g_i, and Q_i, assuming the intrinsic growth function is quartic
+# Input:
+# - m: an SxL matrix; m[i,k] is the kth trait mean of species i
+# - P: an SxLxL array; P[i,k,l] is the (k,l)th covar matrix entry of species i
+# Output:
+# - a list with three named entries:
+#   * b: vector with S entries
+#   * g: an SxL matrix
+#   * Q: an SxLxL array
 b_quartic <- function(m, P) {
-    S <- dim(m)[1]
-    L <- dim(m)[2]
-    b <- rep(0, S)
-    g <- matrix(0, S, L)
-    Q <- array(0, c(S, L, L))
-    for (i in 1:S) {
-        trP <- as.numeric(ifelse(L==1, P[i,,], sum(diag(P[i,,])))) ## trace(P_i)
-        trP2 <- as.numeric(sum(diag(P[i,,]%*%P[i,,]))) ## trace(P_i %*% P_i)
-        mm <- sum(m[i,]^2) ## m_i %*% m_i
-        b[i] <- 1-(trP^2+2*trP2+2*trP*mm+4*m[i,]%*%P[i,,]%*%m[i,]+mm^2)/0.5^4
-        g[i,] <- -4*(trP*m[i,]+mm*m[i,]+2*P[i,,]%*%m[i,])/0.5^4
-        Q[i,,] <- -4*(trP*diag(L)+mm*diag(L)+2*(m[i,]%o%m[i,])+2*P[i,,])/0.5^4
-    }
-    return(list(b=b, g=g, Q=Q))
+  S <- dim(m)[1]
+  L <- dim(m)[2]
+  b <- rep(0, S)
+  g <- matrix(0, S, L)
+  Q <- array(0, c(S, L, L))
+  for (i in 1:S) {
+    trP <- as.numeric(ifelse(L==1, P[i,,], sum(diag(P[i,,])))) # trace(P_i)
+    trP2 <- as.numeric(sum(diag(P[i,,]%*%P[i,,]))) # trace(P_i %*% P_i)
+    mm <- sum(m[i,]^2) # m_i %*% m_i
+    b[i] <- 1-(trP^2+2*trP2+2*trP*mm+4*m[i,]%*%P[i,,]%*%m[i,]+mm^2)/0.5^4
+    g[i,] <- -4*(trP*m[i,]+mm*m[i,]+2*P[i,,]%*%m[i,])/0.5^4
+    Q[i,,] <- -4*(trP*diag(L)+mm*diag(L)+2*(m[i,]%o%m[i,])+2*P[i,,])/0.5^4
+  }
+  list(b=b, g=g, Q=Q)
 }
 
-## apply smoothed step function to array: values less than 0 are set to 0;
-## values between 0 and 1 are set to 10*n^3-15*n^4+6*n^5; and values larger
-## than 1 are set to 1
-## Input:
-## - n: vector or arbitrary array of values
-## Output:
-## - array of values with smoothed step function applied to them
+# apply smoothed step function to array: values less than 0 are set to 0;
+# values between 0 and 1 are set to 10*n^3-15*n^4+6*n^5; and values larger
+# than 1 are set to 1
+# Input:
+# - n: vector or arbitrary array of values
+# Output:
+# - array of values with smoothed step function applied to them
 cutoff <- function(n) {
-    return(ifelse(n<1, (1*(n>0))*(n*n*n*(10+n*(-15+6*n))), 1))
+  ifelse(n<1, (1*(n>0))*(n*n*n*(10+n*(-15+6*n))), 1)
 }
 
-## right-hand side of governing equations
-## Input:
-## - time: moment of time (this is here for compatibility reasons; the
-##         system of equations does not actually depend on time explicitly)
-## - state: the dynamical state variables (densities, trait means, and trait
-##          covariance matrices), packaged into a single vector
-## - pars: list of parameters, with the following elements:
-##         $W: an LxL matrix, the covariance matrix of the competition kernel
-##         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
-##             environmental covariance matrix
-##         $bshape: the name of the function (as a string) to be executed for
-##                  calculating b and g, which depend on the shape of the
-##                  intrinsic growth function. For instance, it can be
-##                  "b_quadratic" or "b_quartic"
-## Output:
-## - time derivatives of the state variables coerced into a single vector
+# right-hand side of governing equations
+# Input:
+# - time: moment of time (this is here for compatibility reasons; the
+#         system of equations does not actually depend on time explicitly)
+# - state: the dynamical state variables (densities, trait means, and trait
+#          covariance matrices), packaged into a single vector
+# - pars: list of parameters, with the following elements:
+#         $W: an LxL matrix, the covariance matrix of the competition kernel
+#         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
+#             environmental covariance matrix
+#         $bshape: the name of the function (as a string) to be executed for
+#                  calculating b and g, which depend on the shape of the
+#                  intrinsic growth function. For instance, it can be
+#                  "b_quadratic" or "b_quartic"
+# Output:
+# - time derivatives of the state variables coerced into a single vector
 eqs <- function(time, state, pars) {
-    S <- dim(pars$E)[1] ## number of species
-    L <- dim(pars$E)[2] ## number of trait dimensions
-    n <- state[1:S] ## population densities
-    m <- matrix(state[(S+1):(S*(L+1))], S, L) ## trait means
-    G <- array(state[(S*(L+1)+1):(S*(L^2+L+1))], c(S, L, L)) ## genetic covars
-    P <- G + pars$E ## total phenotypic covar matrices
-    alpha <- matrix(0, S, S)
-    beta <- array(0, c(S, S, L))
-    Gamma <- array(0, c(S, S, L, L))
-    for (i in 1:S) {
-        for (j in 1:S) {
-            A <- solve(P[i,,]+P[j,,]+pars$W) ## (P_i + P_j + W)^(-1)
-            dm <- m[j,]-m[i,] ## difference of trait means
-            alpha[i,j] <- sqrt(det(pars$W)*det(A))*exp(-dm%*%A%*%dm/2)
-            beta[i,j,] <- (A%*%dm)*alpha[i,j]
-            Gamma[i,j,,] <- (as.numeric(A%*%dm)%o%as.numeric(A%*%dm)-A)*alpha[i,j]
-        }
+  S <- dim(pars$E)[1] # number of species
+  L <- dim(pars$E)[2] # number of trait dimensions
+  n <- state[1:S] # population densities
+  m <- matrix(state[(S+1):(S*(L+1))], S, L) # trait means
+  G <- array(state[(S*(L+1)+1):(S*(L^2+L+1))], c(S, L, L)) # genetic covars
+  P <- G + pars$E # total phenotypic covar matrices
+  alpha <- matrix(0, S, S)
+  beta <- array(0, c(S, S, L))
+  Gamma <- array(0, c(S, S, L, L))
+  for (i in 1:S) {
+    for (j in 1:S) {
+      A <- solve(P[i,,]+P[j,,]+pars$W) # (P_i + P_j + W)^(-1)
+      dm <- m[j,]-m[i,] # difference of trait means
+      alpha[i,j] <- sqrt(det(pars$W)*det(A))*exp(-dm%*%A%*%dm/2)
+      beta[i,j,] <- (A%*%dm)*alpha[i,j]
+      Gamma[i,j,,] <- (as.numeric(A%*%dm)%o%as.numeric(A%*%dm)-A)*alpha[i,j]
     }
-    alphan <- alpha%*%n
-    betan <- matrix(0, S, L)
-    Gamman <- array(0, c(S, L, L))
-    for (k in 1:L) {
-        betan[,k] <- beta[,,k]%*%n
-        for (l in 1:L) Gamman[,k,l] <- Gamma[,,k,l]%*%n
-    }
-    p <- match.fun(pars$bshape)(m, P)
-    dndt <- (p$b-alphan)*n*cutoff(n/(1e-8))
-    dmdt <- matrix(0, S, L)
-    dGdt <- array(0, c(S, L, L))
-    for (i in 1:S) {
-        dmdt[i,] <- G[i,,]%*%(p$g[i,]-betan[i,])
-        dGdt[i,,] <- (G[i,,]%*%(p$Q[i,,]-Gamman[i,,])%*%G[i,,])/2
-    }
-    return(list(c(dndt, dmdt, dGdt)))
+  }
+  alphan <- alpha%*%n
+  betan <- matrix(0, S, L)
+  Gamman <- array(0, c(S, L, L))
+  for (k in 1:L) {
+    betan[,k] <- beta[,,k]%*%n
+    for (l in 1:L) Gamman[,k,l] <- Gamma[,,k,l]%*%n
+  }
+  p <- match.fun(pars$bshape)(m, P)
+  dndt <- (p$b-alphan)*n*cutoff(n/(1e-8))
+  dmdt <- matrix(0, S, L)
+  dGdt <- array(0, c(S, L, L))
+  for (i in 1:S) {
+    dmdt[i,] <- G[i,,]%*%(p$g[i,]-betan[i,])
+    dGdt[i,,] <- (G[i,,]%*%(p$Q[i,,]-Gamman[i,,])%*%G[i,,])/2
+  }
+  list(c(dndt, dmdt, dGdt))
 }
 
-## put the matrix containing the solution of the equations in tidy format
-## Input:
-## - dat: dynamical data returned by the function "run_dynamics()" below
-## - pars: list of parameters, with the following elements:
-##         $W: an LxL matrix, the covariance matrix of the competition kernel
-##         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
-##             environmental covariance matrix
-##         $bshape: the name of the function (as a string) to be executed for
-##                  calculating b and g, which depend on the shape of the
-##                  intrinsic growth function. For instance, it can be
-##                  "b_quadratic" or "b_quartic"
-## Output:
-## - a tibble, organized as follows: each row is a species at a given moment
-##   of time, and the columns are:
-##   * time: the moment of time
-##   * species: the ID of the species in the given row (a positive integer)
-##   * G11, G12, ...: entries of the genetic covariance matrix
-##   * m1, m2, ...: entries of the mean trait vector
-##   * n: population density
-##   * bshape: shape of the intrinsic growth function
-##   * E11, E12, ...: entries of the environmental covariance matrix
-##   * W11, W12, ...: entries of the competition kernel's covariance matrix
-##   * Sinit: the number of species at the initial moment of time
+# put the matrix containing the solution of the equations in tidy format
+# Input:
+# - dat: dynamical data returned by the function "run_dynamics()" below
+# - pars: list of parameters, with the following elements:
+#         $W: an LxL matrix, the covariance matrix of the competition kernel
+#         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
+#             environmental covariance matrix
+#         $bshape: the name of the function (as a string) to be executed for
+#                  calculating b and g, which depend on the shape of the
+#                  intrinsic growth function. For instance, it can be
+#                  "b_quadratic" or "b_quartic"
+# Output:
+# - a tibble, organized as follows: each row is a species at a given moment
+#   of time, and the columns are:
+#   * time: the moment of time
+#   * species: the ID of the species in the given row (a positive integer)
+#   * G11, G12, ...: entries of the genetic covariance matrix
+#   * m1, m2, ...: entries of the mean trait vector
+#   * n: population density
+#   * bshape: shape of the intrinsic growth function
+#   * E11, E12, ...: entries of the environmental covariance matrix
+#   * W11, W12, ...: entries of the competition kernel's covariance matrix
+#   * Sinit: the number of species at the initial moment of time
 organize_results <- function(dat, pars) {
-    S <- dim(pars$E)[1] ## number of species
-    L <- dim(pars$E)[2] ## number of trait dimensions
-    names(dat)[1] <- "time"
-    ind <- 2
+  S <- dim(pars$E)[1] # number of species
+  L <- dim(pars$E)[2] # number of trait dimensions
+  names(dat)[1] <- "time"
+  ind <- 2
+  for (i in 1:S) {
+    names(dat)[ind] <- paste0("n_", i)
+    ind <- ind + 1
+  }
+  for (k in 1:L) {
     for (i in 1:S) {
-        names(dat)[ind] <- paste0("n_", i)
+      names(dat)[ind] <- paste0("m", k, "_", i)
+      ind <- ind + 1
+    }
+  }
+  for (l in 1:L) {
+    for (k in 1:L) {
+      for (i in 1:S) {
+        names(dat)[ind] <- paste0("G", k, l, "_", i)
         ind <- ind + 1
+      }
     }
-    for (k in 1:L) {
-        for (i in 1:S) {
-            names(dat)[ind] <- paste0("m", k, "_", i)
-            ind <- ind + 1
-        }
-    }
+  }
+  dat <- dat %>%
+    gather("variable", "val", 2:ncol(dat)) %>%
+    separate(variable, c("type", "species"), sep="_") %>%
+    mutate(species=as.integer(species)) %>%
+    spread(type, val) %>%
+    mutate(bshape=pars$bshape)
+  for (k in 1:L) {
     for (l in 1:L) {
-        for (k in 1:L) {
-            for (i in 1:S) {
-                names(dat)[ind] <- paste0("G", k, l, "_", i)
-                ind <- ind + 1
-            }
-        }
+      dat[paste0("E", k, l)] <- pars$E[dat$species, k, l]
     }
-    dat <- dat %>%
-        gather("variable", "val", 2:ncol(dat)) %>%
-        separate(variable, c("type", "species"), sep="_") %>%
-        mutate(species=as.integer(species)) %>%
-        spread(type, val) %>%
-        mutate(bshape=pars$bshape)
-    for (k in 1:L) {
-        for (l in 1:L) {
-            dat[paste0("E", k, l)] <- pars$E[dat$species, k, l]
-        }
+  }
+  for (k in 1:L) {
+    for (l in 1:L) {
+      dat[paste0("W", k, l)]=pars$W[k, l]
     }
-    for (k in 1:L) {
-        for (l in 1:L) {
-            dat[paste0("W", k, l)]=pars$W[k, l]
-        }
-    }
-    dat <- dat %>% mutate(Sinit=as.integer(S))
-    return(dat)
+  }
+  mutate(dat, Sinit=as.integer(S))
 }
 
-## solve the ODEs of the eco-evolutionary model
-## Input:
-## - state: the initial conditions, coerced into a single vector
-## - tseq: the time sequence for which results should be returned
-## - pars: list of parameters, with the following elements:
-##         $W: an LxL matrix, the covariance matrix of the competition kernel
-##         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
-##             environmental covariance matrix
-##         $bshape: the name of the function (as a string) to be executed for
-##                  calculating b and g, which depend on the shape of the
-##                  intrinsic growth function. For instance, it can be
-##                  "b_quadratic" or "b_quartic"
-## Output:
-## - the solution converted to a tibble
+# solve the ODEs of the eco-evolutionary model
+# Input:
+# - state: the initial conditions, coerced into a single vector
+# - tseq: the time sequence for which results should be returned
+# - pars: list of parameters, with the following elements:
+#         $W: an LxL matrix, the covariance matrix of the competition kernel
+#         $E: an SxLxL array; E[i,k,l] is the (k,l)th entry of species i's
+#             environmental covariance matrix
+#         $bshape: the name of the function (as a string) to be executed for
+#                  calculating b and g, which depend on the shape of the
+#                  intrinsic growth function. For instance, it can be
+#                  "b_quadratic" or "b_quartic"
+# Output:
+# - the solution converted to a tibble
 run_dynamics <- function(state, tseq, pars) {
-    ode(func=eqs, y=state, parms=pars, times=tseq) %>% ## solve ODEs
-        as.data.frame %>% ## convert to data frame (necessary for next step)
-        as_tibble %>% ## convert to tibble
-        return
+  ode(func=eqs, y=state, parms=pars, times=tseq) %>% # solve ODEs
+    as.data.frame() %>% # convert to data frame (necessary for next step)
+    as_tibble() # convert to tibble
 }
